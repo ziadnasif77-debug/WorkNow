50081465d91a6f38f9459b90334626c32d6fb3cb
ziadnasif77 <ziadnasif77@gmail.com>
2026-04-16 08:23:00 +0200

    fix: try ReactNativeCore path for ReactAppDependencyProvider

--- apps/mobile/plugins/withReactAppDependencyProvider.js
+++ apps/mobile/plugins/withReactAppDependencyProvider.js
@@ -9,9 +9,10 @@ module.exports = function withReactAppDependencyProvider(config) {
       const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
       let podfile = fs.readFileSync(podfilePath, 'utf8');
       if (!podfile.includes('ReactAppDependencyProvider')) {
+        const rnPath = '../../../node_modules/.pnpm/react-native@0.76.3_@babel+core@7.29.0_@babel+preset-env@7.29.2_@babel+core@7.29.0__@react-na_ae3nfj7qc5hydleqc2b5wne7fa/node_modules/react-native';
         podfile = podfile.replace(
           "use_expo_modules!",
-          "pod 'ReactAppDependencyProvider', :path => '../../../node_modules/.pnpm/react-native@0.76.3_@babel+core@7.29.0_@babel+preset-env@7.29.2_@babel+core@7.29.0__@react-na_ae3nfj7qc5hydleqc2b5wne7fa/node_modules/react-native/Libraries/AppDelegate'\n  use_expo_modules!"
+          `pod 'ReactAppDependencyProvider', :path => '${rnPath}/Libraries/ReactNativeCore'\n  use_expo_modules!`
         );
         fs.writeFileSync(podfilePath, podfile);
       }
